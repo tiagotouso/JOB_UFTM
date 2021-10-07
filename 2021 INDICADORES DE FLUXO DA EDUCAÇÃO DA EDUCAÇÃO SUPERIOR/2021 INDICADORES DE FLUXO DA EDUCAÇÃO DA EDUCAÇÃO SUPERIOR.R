88b84c0e6fa3732f424b077e240e0af25285bdc2
tiagotouso <44351630+tiagotouso@users.noreply.github.com>
2021-10-07 09:13:21 -0300

    SUBIR

---
+++ 2021 INDICADORES DE FLUXO DA EDUCAÇÃO DA EDUCAÇÃO SUPERIOR/2021 INDICADORES DE FLUXO DA EDUCAÇÃO DA EDUCAÇÃO SUPERIOR.R
@@ -0,0 +1,206 @@
+# SCRIPT PARA GERAR GRÁFICOS DO FLUXO DA EDUCAÇÃO SUPERIOR
+
+# BASE DE DADOS
+# https://www.gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/indicadores-educacionais/indicadores-de-fluxo-da-educacao-superior
+
+library(readxl)
+library(dplyr)
+library(ggplot2)
+library(ggthemes)
+
+theme_set(theme_economist())
+
+# FUNÇÕES BEGIN ----
+
+criarGraficoGGP <- function(dados, curso, ano, numgrafico){
+  # FUNÇÃO PARA CRIAR O GRÁFICO
+  
+  anomin <- min(dados$ANO)
+  
+  titulo <- paste('Fluxo da Educação Superior - ', curso)
+  
+  dados %>% ggplot(aes(ANO, VALOR, colour = factor(TIPO))) +
+    geom_line() + geom_point() + 
+    labs(title = titulo, caption = 'Fonte: INEP', x = '', y = '', colour = NULL)+
+    theme(plot.title = element_text(hjust = 0.5),
+          legend.position="bottom",
+          color  = NULL, legend.text=element_text(size=8)) +
+    coord_cartesian(ylim = c(0, 105), xlim = c(anomin, 2019)) +
+    scale_x_continuous(breaks=seq(anomin, 2019)) +
+    guides(col = guide_legend(nrow = 2))
+  
+  numtxt <- addCaracteres(numgrafico)
+  narquivo <- paste('_GRAFICOS_R/',numtxt, ano, titulo, '.jpeg')
+  ggsave(narquivo, scale = 1, dpi = 300, width = 20, height = 10, units = "cm")
+}
+
+
+buscasValores <- function(dadosf, co_municipio, no_curso){
+  # FUNÇÃO PARA FILTRA OS VALORES DO CURSO POR CIDADE
+  
+  curso <- dadosf %>% filter(CO_MUNICIPIO == co_municipio & NO_CURSO == no_curso)
+  curso <- curso %>% select('ANO', 'TAP', 'TCA', 'TDA', 'TCAN', 'TADA')
+  
+  ddt <- data.frame()
+  for(cl in c('TAP', 'TCA', 'TDA', 'TCAN', 'TADA')){
+    ax <- curso %>% select(ANO, cl)
+    ax <- rename(ax, VALOR=cl)
+    ax['TIPO'] <- cl
+    
+    ddt <- rbind(ddt, ax)
+  }
+  
+  ddt <- ddt %>% mutate(TIPO = case_when(TIPO == 'TAP' ~ 'Taxa de Permanência',
+                                  TIPO == 'TCA' ~ 'Taxa de Conclusão Acumulada',
+                                  TIPO == 'TDA' ~ 'Taxa de Desistência Acumulada',
+                                  TIPO == 'TCAN' ~ 'Taxa de Conclusão Anual',
+                                  TIPO == 'TADA' ~ 'Taxa de Desistência Anual'))
+
+  return(ddt)
+}
+
+codigox = 100
+
+fd <- function(){
+  
+  print(codigox)
+}
+fd
+
+
+filtraDadosIES <- function(dados){
+  # FUNÇÃO PARA FILTRAR DADOS DA IES
+  
+  # OS NOMES DAS COLUNAS ESTÃO NA LINHA 8
+  colunas <- c(dados[8, ])
+  
+  # CORRIGIR OS NOMES DAS COLUNAS
+  colnames(dados) <- colunas
+  
+  # FILTRA OS DADOS DA UFTM
+  dadosIES <- dados %>% filter(CO_IES == 597)
+  
+  # CORRIGIR OS VALORES NUMÉRICOS
+  dadosIES <- dadosIES %>% 
+    mutate(ANO=as.integer(dadosIES$NU_ANO_REFERENCIA),
+           TAP=as.numeric(dadosIES$TAP),
+           TCA=as.numeric(dadosIES$TCA),
+           TDA=as.numeric(dadosIES$TDA),
+           TCAN=as.numeric(dadosIES$TCAN),
+           TADA=as.numeric(dadosIES$TADA))
+  
+  return(dadosIES)
+}
+
+
+filtrarCursosIES <- function(dadosIES){
+  # CRIA UMA LISTA DE CIDADE E CURSO DO DATASET
+  
+  cursosIES <- dadosIES %>% select(CO_MUNICIPIO, NO_CURSO)
+  cursosIES <- cursosIES[duplicated(cursosIES)== FALSE, ]
+  
+  return(cursosIES)
+}
+
+
+addCaracteres <- function(numero){
+  # FUNÇÃO PARA ADD ZEROS A ESQUERDA
+  
+  valor <- ''
+  for (n in seq(1, 5 - nchar(as.character(numero)))){
+    valor <- paste(valor,  '0', sep='')
+  }
+  valor <- paste(valor,  numero, sep='')
+  
+  return(valor)
+}
+
+# FUNÇÕES END ----
+
+# SCRIPT BEGIN ----
+
+# DIRETÓRIO DOS ARQUIVOS
+diretorio <- 'C:/_BIG_DATA/INDICADORES DE FLUXO DA EDUCAÇÃO DA EDUCAÇÃO SUPERIOR/'
+
+# NOME DOS ARQUIVOS
+arquivos <- c('indicadores_trajetoria_educacao_superior_2015_2019.xlsx',
+    'indicadores_trajetoria_educacao_superior_2014_2019.xlsx',
+    'indicadores_trajetoria_educacao_superior_2013_2019.xlsx',
+    'indicadores_trajetoria_educacao_superior_2012_2019.xlsx',
+    'indicadores_trajetoria_educacao_superior_2011_2019.xlsx',
+    'indicadores_trajetoria_educacao_superior_2010_2019.xlsx')
+
+nomelista <- c('2015_2019', '2014_2019', '2013_2019',
+                    '2012_2019', '2011_2019', '2010_2019')
+
+# CARREGAR ARQUIVOS
+dadoslista <- list()
+for (num in c(1:6)){
+  narquivo <- paste(diretorio, arquivos[num], sep='')
+  dados <- read_excel(narquivo)
+  dadoslista[[nomelista[num]]] <- dados
+}
+
+# FILTRAR DADOS DA IES
+# TRATAR DADOS
+# CRIAR DATASET DOS CURSOS
+
+dadoscursos <- data.frame()
+dadoslistaIES <- list()
+for(ano in nomelista){
+  print(ano)
+  dados <- dadoslista[[ano]]
+  dadosIES <- filtraDadosIES(dados)
+  dadoslistaIES[[ano]] <- dadosIES
+  cursos <- filtrarCursosIES(dadosIES)
+  cursos['ANO'] <- ano
+
+  dadoscursos <- rbind(dadoscursos, cursos)
+}
+
+# DATASET DE TODOS OS ANOS E CURSOS
+dadoscursos <- dadoscursos[duplicated(dadoscursos) == FALSE,]
+dadoscursos <- arrange(dadoscursos, CO_MUNICIPIO, NO_CURSO, ANO)
+
+# DATASET DE TODOS OS CURSOS
+cidades_cursos <- dadoscursos %>% select(CO_MUNICIPIO, NO_CURSO)
+cidades_cursos <- cidades_cursos[duplicated(cidades_cursos) == FALSE,]
+
+# ORGANIZAR OS CURSOS POR INSTITUTOS
+cidades_cursos <- cidades_cursos %>% mutate(INSTITUTO = case_when(
+  CO_MUNICIPIO == 3134400 &  
+    NO_CURSO %in% c('CIÊNCIAS BIOLÓGICAS', 'QUÍMICA') ~ 'ITURAMA',
+  NO_CURSO %in% c('EDUCAÇÃO DO CAMPO', 'FÍSICA', 'MATEMÁTICA', 'QUÍMICA') ~ 'ICENE',
+  NO_CURSO %in% c('BIOMEDICINA', 'CIÊNCIAS BIOLÓGICAS', 'EDUCAÇÃO FÍSICA', 'ENFERMAGEM',
+                  'FISIOTERAPIA', 'MEDICINA', 'NUTRIÇÃO', 'TERAPIA OCUPACIONAL') ~ 'ICS - ICBN',
+  NO_CURSO %in% c('ENGENHARIA AMBIENTAL', 'ENGENHARIA CIVIL', 'ENGENHARIA DE ALIMENTOS',
+                  'ENGENHARIA DE PRODUÇÃO', 'ENGENHARIA ELÉTRICA', 'ENGENHARIA MECÂNICA',
+                  'ENGENHARIA QUÍMICA') ~ 'ICTE',
+  NO_CURSO %in% c('GEOGRAFIA', 'HISTÓRIA', 'LETRAS - PORTUGUÊS E ESPANHOL', 
+                  'LETRAS - PORTUGUÊS E INGLÊS', 'PSICOLOGIA', 'SERVIÇO SOCIAL') ~ 'IELACHS',
+  TRUE~'' ))
+
+cidades_cursos <- arrange(cidades_cursos, CO_MUNICIPIO, INSTITUTO, NO_CURSO)
+
+
+# CRIAR OS GRÁFICOS POR INSTITUTO, CIDADE, CURSO POR ANO
+numerografico <- 1
+for (id in seq(1, dim(cidades_cursos)[1])){
+  cdmunicipio <- as.character(cidades_cursos[id, 1])
+  curso <- as.character(cidades_cursos[id, 2])
+  
+  anos <- dadoscursos %>% filter(CO_MUNICIPIO == cdmunicipio,
+                                 NO_CURSO == curso) %>% select(ANO) %>% arrange(desc(ANO))
+
+  if(length(anos) > 0){
+    for( ano in anos$ANO){
+      dados <- dadoslistaIES[[ano]]
+      ddt <- buscasValores(dados, cdmunicipio, curso)
+      print(curso, ano)
+      criarGraficoGGP(ddt, curso, ano, numerografico)
+      numerografico <- numerografico + 1
+    }
+  }
+}
+
+# SCRIPT END ----
