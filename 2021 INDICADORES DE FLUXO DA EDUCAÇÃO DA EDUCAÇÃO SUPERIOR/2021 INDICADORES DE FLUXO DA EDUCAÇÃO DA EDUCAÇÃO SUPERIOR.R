328e2cdd6b4b284a9eeb68a57246cd6403a994de
tiagotouso <44351630+tiagotouso@users.noreply.github.com>
2021-11-24 22:09:46 -0300

    SUBIR
    SUBIR

--- 2021 INDICADORES DE FLUXO DA EDUCAÇÃO DA EDUCAÇÃO SUPERIOR/2021 INDICADORES DE FLUXO DA EDUCAÇÃO DA EDUCAÇÃO SUPERIOR.R
+++ 2021 INDICADORES DE FLUXO DA EDUCAÇÃO DA EDUCAÇÃO SUPERIOR/2021 INDICADORES DE FLUXO DA EDUCAÇÃO DA EDUCAÇÃO SUPERIOR.R
@@ -6,14 +6,13 @@
 library(readxl)
 library(dplyr)
 library(ggplot2)
-library(ggrepel)
 library(ggthemes)
 
 theme_set(theme_economist())
 
 # FUNÇÕES BEGIN ----
 
-criarGraficoGGP <- function(dados, instituto, curso, ano, pasta){
+criarGraficoGGP <- function(dados, curso, ano, numgrafico){
   # FUNÇÃO PARA CRIAR O GRÁFICO
   
   anomin <- min(dados$ANO)
@@ -21,20 +20,18 @@ criarGraficoGGP <- function(dados, instituto, curso, ano, pasta){
   titulo <- paste('Fluxo da Educação Superior - ', curso)
   
   dados %>% ggplot(aes(ANO, VALOR, colour = factor(TIPO))) +
-    geom_line() +
-    geom_point() +
-    geom_text_repel(aes(label = format(round(VALOR, 2), nsmall = 2)), size=3.0, show_guide = F) + 
+    geom_line() + geom_point() + 
     labs(title = titulo, caption = 'Fonte: INEP', x = '', y = '', colour = NULL)+
     theme(plot.title = element_text(hjust = 0.5),
           legend.position="bottom",
           color  = NULL, legend.text=element_text(size=8)) +
     coord_cartesian(ylim = c(0, 105), xlim = c(anomin, 2019)) +
     scale_x_continuous(breaks=seq(anomin, 2019)) +
-    guides(col = guide_legend(nrow = 1))
+    guides(col = guide_legend(nrow = 2))
   
-  narquivo <- paste('_GRAFICOS_R/', pasta, '/I-F-E-S', sep = '')
-  narquivo <- paste(narquivo,instituto, curso, ano, '.jpeg')
-  ggsave(narquivo, scale = 1, dpi = 300, width = 26, height = 13, units = "cm")
+  numtxt <- addCaracteres(numgrafico)
+  narquivo <- paste('_GRAFICOS_R/',numtxt, ano, titulo, '.jpeg')
+  ggsave(narquivo, scale = 1, dpi = 300, width = 20, height = 10, units = "cm")
 }
 
 
@@ -42,37 +39,33 @@ buscasValores <- function(dadosf, co_municipio, no_curso){
   # FUNÇÃO PARA FILTRA OS VALORES DO CURSO POR CIDADE
   
   curso <- dadosf %>% filter(CO_MUNICIPIO == co_municipio & NO_CURSO == no_curso)
+  curso <- curso %>% select('ANO', 'TAP', 'TCA', 'TDA', 'TCAN', 'TADA')
   
-  if(dim(curso)[1] > 0){
-    curso <- curso %>% select('ANO', 'TAP', 'TCA', 'TDA', 'TCAN', 'TADA')
+  ddt <- data.frame()
+  for(cl in c('TAP', 'TCA', 'TDA', 'TCAN', 'TADA')){
+    ax <- curso %>% select(ANO, cl)
+    ax <- rename(ax, VALOR=cl)
+    ax['TIPO'] <- cl
     
-    ddt <- data.frame()
-    # for(cl in c('TAP', 'TCA', 'TDA', 'TCAN', 'TADA')){
-    for(cl in c('TAP', 'TCA', 'TDA')){
-      ax <- curso %>% select(ANO, cl)
-      ax <- rename(ax, VALOR=cl)
-      ax['TIPO'] <- cl
-      
-      ddt <- rbind(ddt, ax)
-    }
-    
-    ddt <- ddt %>% mutate(TIPO = case_when(TIPO == 'TAP' ~ 'Taxa de Permanência',
-                                           TIPO == 'TCA' ~ 'Taxa de Conclusão Acumulada',
-                                           TIPO == 'TDA' ~ 'Taxa de Desistência Acumulada',
-                                           TIPO == 'TCAN' ~ 'Taxa de Conclusão Anual',
-                                           TIPO == 'TADA' ~ 'Taxa de Desistência Anual'))
-    
-    ddt <- ddt %>% mutate(VALOR = round(VALOR, 2))
-    
-    # RETORNA 1 + DADOS SE O DATASET FOR <> 0
-    retorno <- list(1, ddt)
-  }else{
-    # RETORNA 0 SE O DATASET FOR == 0
-    retorno <- list(0, list(0))
+    ddt <- rbind(ddt, ax)
   }
+  
+  ddt <- ddt %>% mutate(TIPO = case_when(TIPO == 'TAP' ~ 'Taxa de Permanência',
+                                  TIPO == 'TCA' ~ 'Taxa de Conclusão Acumulada',
+                                  TIPO == 'TDA' ~ 'Taxa de Desistência Acumulada',
+                                  TIPO == 'TCAN' ~ 'Taxa de Conclusão Anual',
+                                  TIPO == 'TADA' ~ 'Taxa de Desistência Anual'))
 
-  return(retorno)
+  return(ddt)
 }
+
+codigox = 100
+
+fd <- function(){
+  
+  print(codigox)
+}
+fd
 
 
 filtraDadosIES <- function(dados){
@@ -140,8 +133,7 @@ arquivos <- c('indicadores_trajetoria_educacao_superior_2015_2019.xlsx',
 nomelista <- c('2015_2019', '2014_2019', '2013_2019',
                     '2012_2019', '2011_2019', '2010_2019')
 
-# CARREGAR OS DATASET
-# VARIÁVEL PARA GUARDAR TODOS OS DATASET CARREGADOS
+# CARREGAR ARQUIVOS
 dadoslista <- list()
 for (num in c(1:6)){
   narquivo <- paste(diretorio, arquivos[num], sep='')
@@ -153,11 +145,8 @@ for (num in c(1:6)){
 # TRATAR DADOS
 # CRIAR DATASET DOS CURSOS
 
-# VARIÁVEL PARA GUARDAR DOS OS CURSOS E ANOS DOS DATASET
 dadoscursos <- data.frame()
-# VARIÁVEL PARA GUARDAR TODOS OS DATASET DA INSTITUIÇÃO
 dadoslistaIES <- list()
-
 for(ano in nomelista){
   print(ano)
   dados <- dadoslista[[ano]]
@@ -169,72 +158,49 @@ for(ano in nomelista){
   dadoscursos <- rbind(dadoscursos, cursos)
 }
 
-# CARREGAR DADOS DOS CURSOS DE UBERABA
-# COM A PLANILHA INTERNA COM DADOS DOS CURSOS
-planilhacursos <- read_excel('_DADOS/DADOS DO CURSO PARA CÁLCULO DO ALUNO EQUIVALENTE GRADUAÇÃO.xlsx')
-planilhacursos <- planilhacursos %>% select(INSTITUTO, MUNICIPIO, CURSO, DURAÇÃO)
+# DATASET DE TODOS OS ANOS E CURSOS
+dadoscursos <- dadoscursos[duplicated(dadoscursos) == FALSE,]
+dadoscursos <- arrange(dadoscursos, CO_MUNICIPIO, NO_CURSO, ANO)
 
-planilhacursos <- planilhacursos %>% mutate(CO_MUNICIPIO=case_when(MUNICIPIO == 'UBERABA' ~ 3170107,
-                                       MUNICIPIO == 'ITURAMA' ~ 3134400))
-planilhacursos['ARQUIVO'] <- planilhacursos[,'DURAÇÃO'] + 1 -4
-planilhacursos <- planilhacursos %>% arrange(INSTITUTO, CURSO)
+# DATASET DE TODOS OS CURSOS
+cidades_cursos <- dadoscursos %>% select(CO_MUNICIPIO, NO_CURSO)
+cidades_cursos <- cidades_cursos[duplicated(cidades_cursos) == FALSE,]
+
+# ORGANIZAR OS CURSOS POR INSTITUTOS
+cidades_cursos <- cidades_cursos %>% mutate(INSTITUTO = case_when(
+  CO_MUNICIPIO == 3134400 &  
+    NO_CURSO %in% c('CIÊNCIAS BIOLÓGICAS', 'QUÍMICA') ~ 'ITURAMA',
+  NO_CURSO %in% c('EDUCAÇÃO DO CAMPO', 'FÍSICA', 'MATEMÁTICA', 'QUÍMICA') ~ 'ICENE',
+  NO_CURSO %in% c('BIOMEDICINA', 'CIÊNCIAS BIOLÓGICAS', 'EDUCAÇÃO FÍSICA', 'ENFERMAGEM',
+                  'FISIOTERAPIA', 'MEDICINA', 'NUTRIÇÃO', 'TERAPIA OCUPACIONAL') ~ 'ICS - ICBN',
+  NO_CURSO %in% c('ENGENHARIA AMBIENTAL', 'ENGENHARIA CIVIL', 'ENGENHARIA DE ALIMENTOS',
+                  'ENGENHARIA DE PRODUÇÃO', 'ENGENHARIA ELÉTRICA', 'ENGENHARIA MECÂNICA',
+                  'ENGENHARIA QUÍMICA') ~ 'ICTE',
+  NO_CURSO %in% c('GEOGRAFIA', 'HISTÓRIA', 'LETRAS - PORTUGUÊS E ESPANHOL', 
+                  'LETRAS - PORTUGUÊS E INGLÊS', 'PSICOLOGIA', 'SERVIÇO SOCIAL') ~ 'IELACHS',
+  TRUE~'' ))
+
+cidades_cursos <- arrange(cidades_cursos, CO_MUNICIPIO, INSTITUTO, NO_CURSO)
 
 
-# CRIAR OS GRÁFICOS PARCIAIS POR INSTITUTO, CIDADE, CURSO
-for (num in seq(1, dim(planilhacursos)[1])){
+# CRIAR OS GRÁFICOS POR INSTITUTO, CIDADE, CURSO POR ANO
+numerografico <- 1
+for (id in seq(1, dim(cidades_cursos)[1])){
+  cdmunicipio <- as.character(cidades_cursos[id, 1])
+  curso <- as.character(cidades_cursos[id, 2])
   
-  cdmunicipio = as.character(planilhacursos[num, 'CO_MUNICIPIO'])
-  curso = as.character(planilhacursos[num, 'CURSO'])
-  instituto = as.character(planilhacursos[num, 'INSTITUTO'])
-  
-  print(curso)
+  anos <- dadoscursos %>% filter(CO_MUNICIPIO == cdmunicipio,
+                                 NO_CURSO == curso) %>% select(ANO) %>% arrange(desc(ANO))
 
-  idarquivo = as.numeric(planilhacursos[num, 'ARQUIVO'])
-  ano <- nomelista[idarquivo]
-  dadostemp <- dadoslistaIES[[ano]]
-
-  resultado <- buscasValores(dadostemp, cdmunicipio, curso)
-
-  if (resultado[[1]] == 1){
-    contador <- contador + 1
-    dadosgrafico <- resultado[[2]]
-    criarGraficoGGP(dadosgrafico, instituto, curso, ano, 'PARCIAL')
-  }else{
-    print('--- ERRO ---')
-    print(paste('  ', 'CURSO NÃO ENCONTRADO NO ARQUIVO'))
-    print(paste('  ', curso, ' - ', ano))
-  }
-}
-
-# CRIAR OS GRÁFICOS TOTAIS POR INSTITUTO, CIDADE, CURSO
-for (num in seq(1, dim(planilhacursos)[1])){
-  
-  cdmunicipio = as.character(planilhacursos[num, 'CO_MUNICIPIO'])
-  curso = as.character(planilhacursos[num, 'CURSO'])
-  instituto = as.character(planilhacursos[num, 'INSTITUTO'])
- 
-  filtro <- dadoscursos %>% filter(CO_MUNICIPIO == cdmunicipio
-                                   & NO_CURSO == curso)
-  print(curso)
-  if (dim(filtro)[1] > 0){
-    
-    anos <- filtro[,'ANO']$ANO
-    
-    for (ano in anos){
-      print(paste('  ', ano))
-      dadostemp <- dadoslistaIES[[ano]]
-      resultado <- buscasValores(dadostemp, cdmunicipio, curso)
-      
-      if (resultado[[1]] == 1){
-        contador <- contador + 1
-        dadosgrafico <- resultado[[2]]
-        criarGraficoGGP(dadosgrafico, instituto, curso, ano, 'TOTAL')
-      }
+  if(length(anos) > 0){
+    for( ano in anos$ANO){
+      dados <- dadoslistaIES[[ano]]
+      ddt <- buscasValores(dados, cdmunicipio, curso)
+      print(curso, ano)
+      criarGraficoGGP(ddt, curso, ano, numerografico)
+      numerografico <- numerografico + 1
     }
-  }else{
-    print('--- ERRO ---')
-    print(paste('  ', 'CURSO NÃO ENCONTRADO NO ARQUIVO'))
-    print(paste('  ', curso, ' - ', ano))
   }
 }
 
+# SCRIPT END ----
